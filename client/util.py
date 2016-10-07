1c4c3ecfd5dd948360f13b3d3cdbd2cbdb99c5b7
Christian Ashby <git@cashby.me.uk>
2016-10-07 09:07:23 +0000

    Webpage updates, user and start game journeys now work

---
+++ client/util.py
@@ -0,0 +1,9 @@
+import urllib
+
+def urlPost(url, params=None):
+    if params:
+        f = urllib.urlopen(url, urllib.urlencode(params))
+    else:
+        f = urllib.urlopen(url)
+    out = f.read()
+    return out
